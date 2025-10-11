cbc54ba059590ccbdb2ab545a860065446b7748c
AshleySunsine <sgt16@yandex.ru>
2025-10-12 00:02:12 +0300

    Слои Docker | Оптимизация сборки [#505360]
    Слои Docker | Оптимизация сборки [#505360]
    
    Слои Docker | Оптимизация сборки [#505360]
    
    Слои Docker | Оптимизация сборки [#505360]
    
    Слои Docker | Оптимизация сборки [#505360]
    
    Слои Docker | Оптимизация сборки [#505360]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,13 @@
 rootProject.name = "DevOps"
 buildCache {
     remote<HttpBuildCache> {
-        url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL") ?: "http://192.168.0.189:5071/cache/")
+        url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
         isAllowInsecureProtocol = true
         isAllowUntrustedServer = true
-        val isPushString = System.getenv("GRADLE_REMOTE_CACHE_USH")
-        isPush = isPushString.toBoolean() ?: true
+        isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
         credentials {
-            username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME" ?: "user009")
-            password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD" ?: "ulpuugz7vyymeweqamr2daiide")
+            username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
+            password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
         }
-
     }
 }
