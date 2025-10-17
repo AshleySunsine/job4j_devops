3964b2b828cd53c9b9e7d55b63ecd44ae553dd7b
AshleySunsine <sgt16@yandex.ru>
2025-10-17 21:37:06 +0300

    Структура и написание скриптов миграций [#505365]

--- src/main/resources/db/changelog/changeset/002_ddl_alter_table_add_fields.sql
+++ src/main/resources/db/changelog/changeset/002_ddl_alter_table_add_fields.sql
@@ -1,7 +1,11 @@
 --liquibase formatted sql
---changeset parsentev:add_columns_to_users
+--changeset parsentev:add_first_arg
+ALTER TABLE users ADD COLUMN first_arg VARCHAR(255);
 
-ALTER TABLE users
-    ADD COLUMN first_arg VARCHAR(255),
-    ADD COLUMN second_arg VARCHAR(255),
-    ADD COLUMN result VARCHAR(255);
+--liquibase formatted sql
+--changeset parsentev:add_second_arg
+ALTER TABLE users ADD COLUMN second_arg VARCHAR(255);
+
+--liquibase formatted sql
+--changeset parsentev:add_result
+ALTER TABLE users ADD COLUMN result VARCHAR(255);
