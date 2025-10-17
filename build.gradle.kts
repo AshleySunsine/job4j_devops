99c76dd941c118bd0a42384aabc9c5ed1d89fbd4
AshleySunsine <sgt16@yandex.ru>
2025-10-17 19:45:50 +0300

    Liquibase в Gradle [#505364]

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
     id("com.github.spotbugs") version "6.0.26"
+    id("org.liquibase.gradle") version "3.0.1"
 }
 
 group = "ru.job4j.devops"
@@ -53,6 +54,50 @@ dependencies {
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly(libs.junit.platform.launcher)
     implementation(libs.assertj.core)
+    liquibaseRuntime("org.liquibase:liquibase-core:4.30.0")
+    liquibaseRuntime("org.postgresql:postgresql:42.7.4")
+    liquibaseRuntime("javax.xml.bind:jaxb-api:2.3.1")
+    liquibaseRuntime("ch.qos.logback:logback-core:1.5.15")
+    liquibaseRuntime("ch.qos.logback:logback-classic:1.5.15")
+    liquibaseRuntime("info.picocli:picocli:4.6.1")
+    testImplementation ("com.h2database:h2")
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.liquibase:liquibase-core:4.30.0")
+    }
+}
+/*
+liquibase {
+    activities.register("main") {
+        this.arguments = mapOf(
+                "logLevel"       to "info",
+                "url"            to "jdbc:postgresql://192.168.0.189:5432/job4j_devops",
+                "username"       to "devops",
+                "password"       to "1111",
+                "classpath"      to "src/main/resources",
+                "changelogFile"  to "db/changelog/db.changelog-master.xml"
+        )
+    }
+    runList = "main"
+}
+*/
+liquibase {
+    activities.register("main") {
+        this.arguments = mapOf(
+                "logLevel"       to "info",
+                "url"            to "jdbc:h2:mem:testdb",
+                "username"       to "sa",
+                "password"       to "",
+                "classpath"      to "src/main/resources",
+                "changelogFile"  to "db/changelog/db.changelog-master.xml"
+        )
+    }
+    runList = "main"
 }
 
 tasks.withType<Test> {
